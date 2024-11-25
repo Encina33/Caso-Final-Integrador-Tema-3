1044aaa17bd3946361adc774eb1c2e0679f00ebe
Encina Fernández Rodríguez <mrencnfr@myuax.com>
2024-11-25 16:59:54 +0100

    kk

---
+++ cmake-build-debug/CMakeFiles/Caso_Final_Integrador_Tema_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caso_Final_Integrador_Tema_3.dir/main.c.obj"
+  "CMakeFiles/Caso_Final_Integrador_Tema_3.dir/main.c.obj.d"
+  "Caso_Final_Integrador_Tema_3.exe"
+  "Caso_Final_Integrador_Tema_3.exe.manifest"
+  "Caso_Final_Integrador_Tema_3.pdb"
+  "libCaso_Final_Integrador_Tema_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Caso_Final_Integrador_Tema_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
